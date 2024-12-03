4cd02aa8b20e47a60816ec4f394e596d3264e4a8
juanjo <j.hernandezgarrido@gerimedica.nl>
2024-12-03 08:44:44 +0100

    fix: trade amount was a string

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ KRAKEN_API_KEY = os.getenv("KRAKEN_API_KEY")
 KRAKEN_API_SECRET = os.getenv("KRAKEN_API_SECRET")
 
 PAIR = os.getenv("PAIR")
-TRADE_AMOUNT = os.getenv("TRADE_AMOUNT")
+TRADE_AMOUNT = float(os.getenv("TRADE_AMOUNT", 10))
 
 # RSI (Relative Strength Index) Configuration
 RSI_PERIOD = 10  # Number of periods used for calculating RSI
